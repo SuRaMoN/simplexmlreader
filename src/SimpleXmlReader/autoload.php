53ded9d72400bf55b1f5985a6fcaa38465088714
matthias <matthias@icontroller.be>
2016-08-09 13:51:54 +0200

    Fixed autoloading issue

--- src/SimpleXmlReader/autoload.php
+++ src/SimpleXmlReader/autoload.php
@@ -7,8 +7,9 @@
 spl_autoload_register(function ($class) {
     $availableClasses = array(
         'SimpleXmlReader\ExceptionThrowingXMLReader',
-        'SimpleXmlReader\SimpleXmlReader',
         'SimpleXmlReader\PathIterator',
+        'SimpleXmlReader\SimpleXmlReader',
+        'SimpleXmlReader\XmlException',
     );
     if (in_array($class, $availableClasses)) {
         require(__DIR__ . '/../' . strtr($class, '\\', '/') . '.php');
